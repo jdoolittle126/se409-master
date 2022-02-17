06da82979242ef70a9e4dd122a9bedb34bb5e4ef
Jonathan Doolittle <jdoolittle126@gmail.com>
2022-02-16 19:19:48 -0500

    WorldTrotter Week 6

--- Swift Apps/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Swift Apps/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -39,8 +39,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         
-        if let text = textField.text, let value = Double(text) {
-        fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        if let text = textField.text, let number = numberFormatter.number(from: text) {
+        fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
         } else {
         fahrenheitValue = nil
         }
@@ -62,8 +62,11 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         let hasAlphabeticCharacters = string.rangeOfCharacter(from: CharacterSet.letters)
-        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
-        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        let currentLocale = Locale.current
+        let decimalSeparator = currentLocale.decimalSeparator ?? "."
+        let existingTextHasDecimalSeparator
+        = textField.text?.range(of: decimalSeparator)
+        let replacementTextHasDecimalSeparator = string.range(of: decimalSeparator)
         if (existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil) || hasAlphabeticCharacters != nil  {
             return false
         } else {
