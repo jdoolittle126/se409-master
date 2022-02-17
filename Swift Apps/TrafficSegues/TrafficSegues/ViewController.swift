50cbc56309b2c26e860e21d294be0803305b1da0
Jonathan Doolittle <jdoolittle126@gmail.com>
2022-02-16 20:14:12 -0500

    In Class Lab

---
+++ Swift Apps/TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Doolittle, Jonathan J on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segueSwitch: UISwitch!
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+
+
+}
+
